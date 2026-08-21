e67a393e69d950794611c66b9d3f2389f83358b5
Anderdingus11 <adrian.mosquera@active.ch>
2026-08-21 16:25:11 +0200

    My first upload

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hallo, Welt!");
+Console.WriteLine("Ich heisse Adrian Mosquera Vieiera, bin 15 Jahren alt und bin an der IMS gekommen um über die Informatik und über das Programmieren zu lernen.");
+Console.WriteLine("Dies ist mein erstes Programm, und ich hoffe das ich in der Zukunft mehr machen kann als nur einen Text zu schreiben");
+Console.WriteLine("Doch das wars den heute für mich. Schönen Tag noch!");
