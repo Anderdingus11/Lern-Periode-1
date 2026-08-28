2b1c99ac0471ece4e3378976c7ce917a85c89bf6
Anderdingus11 <adrian.mosquera@active.ch>
2026-08-28 17:00:42 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hallo, Welt!");
-Console.WriteLine("Ich heisse Adrian Mosquera Vieiera, bin 15 Jahren alt und bin an der IMS gekommen um über die Informatik und über das Programmieren zu lernen.");
-Console.WriteLine("Dies ist mein erstes Programm, und ich hoffe das ich in der Zukunft mehr machen kann als nur einen Text zu schreiben");
-Console.WriteLine("Doch das wars den heute für mich. Schönen Tag noch!");
+﻿Random Würfel = new Random();
+int Zahl;
+
+Zahl = Würfel.Next(1, 100);
+Console.WriteLine($"Ich habe eine zufällige Zahl in meinen Kopf und sie ist {Zahl}!");
+string Guess;
+Guess = Console.ReadLine();
